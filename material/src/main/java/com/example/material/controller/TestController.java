c7f0660cc013218c111bc3be23cda5caba4af55e
jiangzhihao <jiangzhihao@auxgroup.com>
2019-11-28 17:46:23 +0800

    start material module

--- material/src/main/java/com/example/material/controller/TestController.java
+++ material/src/main/java/com/example/material/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.example.configserver.controller;
+package com.example.material.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,5 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${name}")
+    String test;
+    @GetMapping("/test")
+    public String test(){
 
+        return test;
+    }
 }
